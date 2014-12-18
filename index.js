4726f48a219e304a95dc4703c7e132a1fc2faf9b
Dan Bucholtz <dan.bucholtz@gmail.com>
2014-12-18 16:46:58 -0600

    testing...

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
 var express = require('express');
 var http = require('http');
+var passport = require('passport');
+var  BearerStrategy = require('passport-http-bearer').Strategy;
+
+var userServiceApi = require('soa-example-user-service-api');
+var utils = require('soa-example-core-utils');
 
 var app = express();
 
-var createServer = function(port){
+passport.use(new BearerStrategy({}, function(token, done) {
+	process.nextTick(function () {
+		userServiceApi.getUserByToken(token).then(function(user){
+			if ( !user ){
+				return done(null, false, {message: "Unknown access token: " + token});
+			}
+			return done(null, user);
+		});
+	});
+}));
+
+
+var createApiServer = function(port){
 	app.configure(function(){
 		app.set('port', port  );
+		app.use(passport.initialize());
 		app.use(express.logger('dev'));
 		app.use(express.bodyParser({uploadDir:'./uploads', limit: '50mb'}));
 		app.use(express.json({limit: '50mb'}));
 		app.use(express.urlencoded({limit: '50mb'}));
 		app.use(express.methodOverride());
 		app.use(express.cookieParser('asd;lfkajs;ldfkj'));
-		app.use(express.session({
-			secret: 'banana',
-			maxAge  : new Date(Date.now() + 360000000),
-			expires : new Date(Date.now() + 360000000)
-		}));
-		app.set('views', __dirname + '/views');
-		app.set('view engine', 'ejs');
-		app.engine('html', require('ejs').renderFile);
-		app.use(express.static(__dirname + '/public'));
 		app.use(app.router);
 	});
 
@@ -37,7 +46,18 @@ var getExpressApp = function(){
 	return app;
 };
 
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	else {
+		res.statusCode = 401;
+		res.send({success:false, errorMessage:"Invalid Access Token"});.
+	}
+}
+
 module.exports = {
-	createServer : createServer,
-	getExpressApp : getExpressApp
+	createApiServer : createApiServer,
+	getExpressApp : getExpressApp,
+	ensureAuthenticated: ensureAuthenticated
 }
