7a4f52341e0f68abc394b824f3145a0eab41a9c5
Dan Bucholtz <dan.bucholtz@gmail.com>
2015-01-03 03:31:25 -0600

    untested

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var BasicStrategy = require('passport-http').BasicStrategy;
 
 var authenticationService = require("soa-example-authentication-service-api");
 var authorizationService = require("soa-example-authorization-service-api");
+var bannedIpService = require("soa-example-banned-ip-service-api");
 var userServiceApi = require('soa-example-user-service-api');
 var utils = require('soa-example-core-utils');
 
@@ -15,11 +16,9 @@ var app = express();
 passport.use(new BearerStrategy({}, function(token, done) {
 	process.nextTick(function () {
 
-		// the user was not in redis or was invalid json or something
-		// get the user
 		userServiceApi.getUserByToken(token).then(function(user){
 			if ( !user ){
-				return done(null, false, {message: "Unknown access token: " + token});
+				return done(null, false, {type: "bearer", token: token, message: "Unknown access token: " + token});
 			}
 			return done(null, user);
 		});
@@ -30,7 +29,7 @@ passport.use(new BasicStrategy({}, function(username, password, done) {
 	process.nextTick(function () {
 		authenticationService.authenticateUserByEmailAddressAndPassword(username, password).then(function(response){
 			if ( !response.success ){
-				return done(null, false, {message: "Unknown User: " + username} );
+				return done(null, false, {type: "basic", username: username, password: password, message: "Unknown User: " + username} );
       		}
       		var user = response.user;
       		authorizationService.getPermissions(user.accessToken).then(function(permissions){
@@ -74,29 +73,47 @@ function ensureAuthenticated(req, res, next) {
 	if ( req.isAuthenticated() ){
 		return next();
 	}
-	// check for the basic auth header
-	if ( req.headers && req.headers.authorization && req.headers.authorization.indexOf("Basic ") >= 0 ){
-		// support basic, too
-		passport.authenticate('basic', { session: false }, function(err, user){
-			if (user) {
-				req.user = user;
-				return next();
+	
+	// check if the ip address making request is banned
+	var ipAddress = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+	bannedIpService.isIpAddressBanned(ipAddress).then(function(banned){
+		if ( banned ){
+			res.statusCode = 401;
+			res.send({success:false, errorMessage:"This IP Address is temporarily banned.  Try again later."});
+			next("This IP Address is temporarioy banned.  Try again later.");
+		}
+		else{
+			// start checkinging headers for authentication
+			if ( req.headers && req.headers.authorization && req.headers.authorization.indexOf("Basic ") >= 0 ){
+				// support basic, too
+				passport.authenticate('basic', { session: false }, function(err, user){
+					if (user) {
+						req.user = user;
+						return next();
+					}
+					else{
+						bannedIpService.invalidBasicCredentials(ipAddress, err.username, err.password);
+					}
+				})(req, res, next);
 			}
-		})(req, res, next);
-	}
-	else if ( req.headers && req.headers.authorization && req.headers.authorization.indexOf("Bearer ") >= 0 ){
-		passport.authenticate('bearer', { session: false }, function (err, user) {
-			if (user) {
-				req.user = user;
-				return next();
+			else if ( req.headers && req.headers.authorization && req.headers.authorization.indexOf("Bearer ") >= 0 ){
+				passport.authenticate('bearer', { session: false }, function (err, user) {
+					if (user) {
+						req.user = user;
+						return next();
+					}
+					else{
+						bannedIpService.invalidBearerCredentials(ipAddress, err.token);
+					}
+				})(req, res, next);
 			}
-		})(req, res, next);
-	}
-	else{
-		res.statusCode = 401;
-		res.send({success: false, errorMessage:"A Bearer Token is required"});
-		next("Missing Bearer Token");
-	}
+			else{
+				res.statusCode = 401;
+				res.send({success: false, errorMessage:"A Bearer Token is required"});
+				next("Missing Bearer Token");
+			}
+		}
+	});
 }
 
 module.exports = {
